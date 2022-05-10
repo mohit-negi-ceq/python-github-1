c04ba72b130819ca5b7fb00ba78a30f65d728630
mohit <mohit.negi@cloudeq.com>
2022-05-10 19:12:55 +0530

    changes

--- python1.py
+++ python1.py
@@ -1,9 +1,12 @@
 # Python program to find the largest number among the three input numbers
 # take three numbers from user
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-num3 = float(input("Enter third number: "))
- 
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# num3 = float(input("Enter third number: "))
+import sys
+num1=sys.argv[1] 
+num2=sys.argv[2]
+num3=sys.argv[3]
 if (num1 > num2) and (num1 > num3):
    largest = num1
 elif (num2 > num1) and (num2 > num3):
